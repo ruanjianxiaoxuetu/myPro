20b6dd89fa562a9a975d0821ea32bedbf4bf977a
wubao <824393972@qq.com>
2017-11-18 12:16:26 +0800

    add User.java

---
+++ src/cn/smbms/pojo/User.java
@@ -0,0 +1,36 @@
+package cn.smbms.pojo;
+
+public class User {
+	private Integer id;
+	private String userCode;
+	private String userName;
+	private String userPassword;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+
+}
